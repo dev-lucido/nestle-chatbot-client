96d2786207226c574e9c57ded6ccb5a886beb056
dev-lucido <pasindu@lucido.life>
2026-04-20 16:56:19 +0530

    hash

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as BrowserRouter, Route, Routes } from "react-router-dom";
 import Mamabot from "./pages/Mamabot/Mamabot.tsx";
 import Namegenerator from "./pages/Namegenerator/Namegenerator.tsx";
 
